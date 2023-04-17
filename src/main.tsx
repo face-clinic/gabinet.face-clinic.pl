290dd06458724c9ab038a41a81c9b0872234e559
Bartosz Gałek <bartosz@galek.com.pl>
2023-04-17 20:00:36 +0200

    colors added

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { MantineProvider } from "@mantine/core";
 import { ModalsProvider } from "@mantine/modals";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-await navigator.wakeLock.request("screen").then(() => console.log('screen wakeLock acquired!'));
+navigator.wakeLock.request("screen").then(() => console.log('screen wakeLock acquired!'));
 
 ReactDOM.createRoot(document.body as HTMLElement).render(
     <React.StrictMode>
